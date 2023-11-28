fe12470c2dfd1d15325d85e3f0c2f2126668e984
jonathan-webdevia <jonathan.lauer@webdevia.fr>
2023-11-28 13:04:04 +0100

    final beta

--- src/pages/Housing/index.jsx
+++ src/pages/Housing/index.jsx
@@ -1,53 +1,75 @@
-import { useParams } from "react-router-dom";
-import datas from "../../assets/data/data";
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useParams, useNavigate } from "react-router-dom";
+import React from "react";
+import { useState } from "react";
+import { useEffect } from "react";
+import axios from "axios";
+
 import Carousel from "../../components/Carousel";
 import Collapse from "../../components/collapse";
 import Rate from "../../components/Rate";
 
 function Housing() {
   const params = useParams();
-  const location = datas.find((data) => data.id === params.id);
+  const [location, setPickedAppart] = useState();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const getData = async () => {
+      const res = await axios.get("/logements.json");
+      const picked = res.data.find((res) => res.id === params.id);
+      res.data.map(() => setPickedAppart(picked));
+      if (picked === undefined) {
+        navigate("/Error");
+      }
+    };
+    getData();
+  }, []);
 
   return (
-    <div className="location">
-      <Carousel slides={location.pictures} />
-      <div className="infoLocation">
-        <div>
-          <h1>{location.title}</h1>
-          <h2>{location.location}</h2>
-          <ul className="tagsContainer">
-            {location.tags.map((tag, index) => (
-              <li key={`${tag}-${index}`} className="tag">
-                {tag}
-              </li>
-            ))}
-          </ul>
-        </div>
-        <div className="hostContainer">
-          <div className="host">
-            <p>
-              {`${location.host.name.split(" ")[0]}`}
-              <br />
-              {`${location.host.name.split(" ")[1]}`}
-            </p>
-            <img src={location.host.picture} alt="l'hôte"></img>
+    <React.StrictMode>
+      {location && (
+        <div className="location">
+          <Carousel slides={location.pictures} />
+          <div className="infoLocation">
+            <div>
+              <h1>{location.title}</h1>
+              <h2>{location.location}</h2>
+              <ul className="tagsContainer">
+                {location.tags.map((tag, index) => (
+                  <li key={`${tag}-${index}`} className="tag">
+                    {tag}
+                  </li>
+                ))}
+              </ul>
+            </div>
+            <div className="hostContainer">
+              <div className="host">
+                <p>
+                  {`${location.host.name.split(" ")[0]}`}
+                  <br />
+                  {`${location.host.name.split(" ")[1]}`}
+                </p>
+                <img src={location.host.picture} alt="l'hôte"></img>
+              </div>
+              <Rate score={location.rating} />
+            </div>
           </div>
-          <Rate score={location.rating} />
+          <div className="collapsesContainer">
+            <Collapse
+              title="Description"
+              content={location.description}
+              type={"string"}
+            />
+            <Collapse
+              title="Equipements"
+              content={location.equipments}
+              type={"list"}
+            />
+          </div>
         </div>
-      </div>
-      <div className="collapsesContainer">
-        <Collapse
-          title="Description"
-          content={location.description}
-          type={"string"}
-        />
-        <Collapse
-          title="Equipements"
-          content={location.equipments}
-          type={"list"}
-        />
-      </div>
-    </div>
+      )}
+    </React.StrictMode>
   );
 }
 
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,28 +1,44 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useEffect } from "react";
 import React from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import Banner from "../../components/Banner/banner";
 import bannerIMG from "../../assets/media/bannerhome.png";
-import datas from "../../assets/data/data";
+import axios from "axios";
 
 function Home() {
+  const [datas, setDatas] = useState();
+  useEffect(() => {
+    const getData = async () => {
+      const response = await axios.get("/logements.json");
+      setDatas(response.data);
+      console.log(datas);
+    };
+    getData();
+  }, []);
   return (
-    <div className="home">
-      <Banner imgSRC={bannerIMG} text="Chez vous, partout et ailleurs" />
-      <div className="cardContainer">
-        {datas.map((data, index) => {
-          return (
-            <Link
-              key={`${data.id}-${index}`}
-              to={`/Housing/${data.id}`}
-              className="card"
-            >
-              <h2>{data.title}</h2>
-              <img src={data.cover} alt="location"></img>
-            </Link>
-          );
-        })}
-      </div>
-    </div>
+    <React.StrictMode>
+      {datas && (
+        <div className="home">
+          <Banner imgSRC={bannerIMG} text="Chez vous, partout et ailleurs" />
+          <div className="cardContainer">
+            {datas.map((data, index) => {
+              return (
+                <Link
+                  key={`${data.id}-${index}`}
+                  to={`/Housing/${data.id}`}
+                  className="card"
+                >
+                  <h2>{data.title}</h2>
+                  <img src={data.cover} alt="location"></img>
+                </Link>
+              );
+            })}
+          </div>
+        </div>
+      )}
+    </React.StrictMode>
   );
 }
 
